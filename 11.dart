0f6fac986b6334678cd5acac3cdedbd075d11b5e
Jesbinjose <jesbinjose57@gmail.com>
2024-01-31 16:36:51 +0530

    11

--- 11.dart
+++ 11.dart
@@ -4,10 +4,10 @@ class Solution {
   int maxArea(List<int> h) {
     int maxA = 0;
     int left = 0;
-    int right = h.length-1;
-    while(left<right){
-      maxA = max((right -left)* min(h[right],h[left]),maxA);
-      h[right]<h[left]?right--:left++;
+    int right = h.length - 1;
+    while (left < right) {
+      maxA = max((right - left) * min(h[right], h[left]), maxA);
+      h[right] < h[left] ? right-- : left++;
     }
     return maxA;
   }
