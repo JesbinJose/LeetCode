0e772931486fd5b802b7910a05bd9ea06a601bd7
Jesbinjose <jesbinjose57@gmail.com>
2024-02-01 18:29:52 +0530

    206

--- 206.dart
+++ 206.dart
@@ -3,13 +3,28 @@ class ListNode {
   ListNode? next;
   ListNode([this.val = 0, this.next]);
 }
-
-class Solution {
+//Recurction
+class SolutionR {
   ListNode? reverseList(ListNode? head) {
     if (head?.next == null) return head;
     ListNode? reversedList = reverseList(head!.next);
     head.next!.next = head;
     head.next = null;
     return reversedList;
+  }
+}
+
+//Iteartion
+class SolutionI {
+  ListNode? reverseList(ListNode? head) {
+    ListNode? prev = null;
+    if (head == null) return null;
+    while (head != null) {
+      ListNode? temp = head.next;
+      head.next = prev;
+      prev = head;
+      head = temp;
+    }
+    return prev;
   }
 }
