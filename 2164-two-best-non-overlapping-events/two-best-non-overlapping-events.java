93dbde6eabffcc5da48109181cb16a96440f0f79
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-03-14 23:30:37 +0530

    Time: 36 ms (98.98%) | Memory: 85.8 MB (100.00%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,13 +1,25 @@
 class Solution {
-  public int maxTwoEvents(int[][] events) {
-    int res = 0, maxVal = 0;
-    PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-    Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
-    for (final int[] e : events) {
-      while (!pq.isEmpty() && pq.peek()[0] < e[0]) maxVal = Math.max(maxVal, pq.poll()[1]);
-      res = Math.max(res, maxVal + e[2]);
-      pq.offer(new int[]{e[1], e[2]});
+  public int maxTwoEvents(int[][] A) {
+    int n = A.length;
+    Arrays.sort(A, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+    int[] maxRight = new int[n];
+    maxRight[n - 1] = A[n - 1][2];
+    for(int i = n - 2; i >= 0; i--) maxRight[i] = Math.max(maxRight[i + 1], A[i][2]);
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+      int endTime = A[i][1];
+      int l = i + 1, r = n - 1;
+      int idx = -1;
+      while(l <= r){
+        int mid = l + (r - l) / 2;
+        if(A[mid][0] > endTime){
+          idx = mid;
+          r = mid - 1;
+        }else l = mid + 1;
+      }
+      if(idx == -1) ans = Math.max(ans, A[i][2]);
+      else ans = Math.max(ans, A[i][2] + maxRight[idx]);
     }
-    return res;
+    return ans;
   }
 }
