533bd63e1d170c8fbb372e3e49f2639b7f778805
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-03-16 08:21:30 +0530

    Time: 324 ms (96.84%) | Memory: 149.3 MB (47.37%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.dart
@@ -0,0 +1,14 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    int pre = -1000000000,res = 0;
+    bool cu = false;
+    for(int i in nums){
+      if(cu&&pre==i)continue;
+      if(pre==i)cu = true;
+      else cu = false;
+      pre = i;
+      nums[res++] = i;
+    }
+    return res;
+  }
+}
