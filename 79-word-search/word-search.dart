45fe31075217507b97513081311c03cd81ea7ae2
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-03 18:32:11 +0530

    Time: 502 ms (100.00%) | Memory: 148.2 MB (88.64%) - LeetSync

--- 79-word-search/word-search.dart
+++ 79-word-search/word-search.dart
@@ -1,18 +1,22 @@
 class Solution {
   bool exist(List<List<String>> board, String word) {
-    Map<String,bool?> map = {};
-    bool rec(int row,int column,int index){
-      if(index>=word.length) return true;
-      if(0>row||row>=board.length||0>column||column>=board[0].length)return false;
-      if(board[row][column]!=word[index] || map['$row,$column']!=null)return false;
-      map['$row,$column'] = true;
-      final bool res = rec(row+1,column,index+1) || rec(row,column+1,index+1) || rec(row-1,column,index+1) || rec(row,column-1,index+1);
-      map['$row,$column'] = null;
-      return res;
+    bool rec(int row, int column, int index) {
+      if (index >= word.length) return true;
+      if (row < 0 || row >= board.length || column < 0 || column >= board[row].length) return false;
+      if (board[row][column] != word[index]) return false;
+      final String original = board[row][column];
+      board[row][column] = ''; 
+      bool found = rec(row + 1, column, index + 1) ||
+          rec(row, column + 1, index + 1) ||
+          rec(row - 1, column, index + 1) ||
+          rec(row, column - 1, index + 1);
+      board[row][column] = original; 
+      return found;
     }
-    for(int i =0;i<board.length;i++)
-      for(int j =0;j<board[i].length;j++)
-        if(board[i][j]==word[0]&&rec(i,j,0)) return true;
+    
+    for (int i = 0; i < board.length; i++) 
+      for (int j = 0; j < board[i].length; j++) 
+        if (board[i][j] == word[0] && rec(i, j, 0)) return true;
     return false;
   }
 }
