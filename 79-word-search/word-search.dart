d9a9493d9023dc338840ef36e8d9dab205c9fe77
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-03 18:28:47 +0530

    Time: 1819 ms (29.55%) | Memory: 152.9 MB (29.55%) - LeetSync

--- 79-word-search/word-search.dart
+++ 79-word-search/word-search.dart
@@ -1,36 +1,18 @@
 class Solution {
-  late List<List<String>> board;
-  late String word;
-  late int m, n, s;
-
-  bool search(int x, int y, int step) {
-    if ((x < 0) || (y < 0) || (x == m) || (y == n) ||
-        (board[x][y] != word[step]))
-            return false;
-    if (++step == s)
-        return true;
-    
-    final c = board[x][y];
-    board[x][y] = '';
-    final result = search(x + 1, y, step) || search(x - 1, y, step) ||
-        search(x, y + 1, step) || search(x, y - 1, step);
-    board[x][y] = c;
-    
-    return result;
-  }
-  
   bool exist(List<List<String>> board, String word) {
-    this.board = board;
-    this.word = word;
-    m = board.length;
-    n = board.first.length;
-    s = word.length;
-
-    for (int i = 0; i != this.m; i++)
-        for (int j = 0; j != this.n; j++)
-            if (search(i, j, 0))
-                return true;
-    
+    Map<String,bool?> map = {};
+    bool rec(int row,int column,int index){
+      if(index>=word.length) return true;
+      if(0>row||row>=board.length||0>column||column>=board[0].length)return false;
+      if(board[row][column]!=word[index] || map['$row,$column']!=null)return false;
+      map['$row,$column'] = true;
+      final bool res = rec(row+1,column,index+1) || rec(row,column+1,index+1) || rec(row-1,column,index+1) || rec(row,column-1,index+1);
+      map['$row,$column'] = null;
+      return res;
+    }
+    for(int i =0;i<board.length;i++)
+      for(int j =0;j<board[i].length;j++)
+        if(board[i][j]==word[0]&&rec(i,j,0)) return true;
     return false;
   }
 }
