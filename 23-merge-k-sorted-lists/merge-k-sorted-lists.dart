3b97c08c40c88207afd103a1c3d61d3977b3d005
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-03 19:30:25 +0530

    Time: 381 ms (93.10%) | Memory: 151.7 MB (58.62%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.dart
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.dart
@@ -1,28 +1,35 @@
 class Solution {
-  ListNode? mergeKLists(List<ListNode?> lists) {
-    ListNode? dummy = ListNode(0); 
-    ListNode? temp = dummy; 
-    lists.removeWhere((item) => item == null);
-    while (lists.isNotEmpty) {
-      int index = 0; 
-      int minValue = lists[0]!.val; 
+  ListNode? mergeTwoLists(ListNode? l1, ListNode? l2) {
+    ListNode? dummy = ListNode(0); // Dummy node to simplify the merging process
+    ListNode? temp = dummy; // Pointer to the current node in the merged list
 
-      for (int i = 1; i < lists.length; i++) {
-        if (lists[i]!.val < minValue) {
-          minValue = lists[i]!.val;
-          index = i;
-        }
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        temp!.next = l1;
+        l1 = l1.next;
+      } else {
+        temp!.next = l2;
+        l2 = l2.next;
       }
-      temp!.next = lists[index];
       temp = temp.next;
-
-      lists[index] = lists[index]!.next;
-
-      if (lists[index] == null) {
-        lists.removeAt(index);
-      }
     }
 
-    return dummy.next; 
+    temp!.next = l1 ?? l2; // Attach the remaining nodes
+
+    return dummy.next; // Return the merged list
+  }
+
+  ListNode? mergeKLists(List<ListNode?> lists) {
+    if (lists.isEmpty) return null; // Handle edge case
+
+    int interval = 1; // Initialize interval to merge lists
+    while (interval < lists.length) {
+      for (int i = 0; i + interval < lists.length; i += interval * 2) {
+        lists[i] = mergeTwoLists(lists[i], lists[i + interval]); // Merge two lists
+      }
+      interval *= 2; // Double the interval for next iteration
+    }
+
+    return lists[0]; // Return the merged list
   }
 }
