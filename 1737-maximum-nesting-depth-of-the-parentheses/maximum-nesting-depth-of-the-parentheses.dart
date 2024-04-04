c162cb2c6e54b9f39504388739f503fd7be26d04
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-04 10:35:56 +0530

    Time: 311 ms (66.67%) | Memory: 151.9 MB (33.33%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.dart
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.dart
@@ -1,9 +1,11 @@
 class Solution {
   int maxDepth(String s) {
     int maxDepthOFString = 0;
-    for(int i =0,depth =0;i<s.length;i++,maxDepthOFString = max(maxDepthOFString,depth))
+    for(int i =0,depth =0;i<s.length;i++){
       if(s[i]=='(') depth++;
       else if(s[i]==')') depth--;
+      if(maxDepthOFString<depth) maxDepthOFString = depth;
+    }
     return maxDepthOFString;
   }
 }
