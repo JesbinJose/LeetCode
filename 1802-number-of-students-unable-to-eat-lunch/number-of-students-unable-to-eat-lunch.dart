cbb149f0acdddd02fa47949e8bd1effe3267a432
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-08 21:02:54 +0530

    Time: 341 ms (60.00%) | Memory: 152.2 MB (20.00%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.dart
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.dart
@@ -1,13 +1,14 @@
 class Solution {
   int countStudents(List<int> students, List<int> sandwiches) {
     int i = 0 , j = 0;
+    Map<int,bool> map ={};
     while(j<students.length&&i<sandwiches.length){
-      if(students[j]==sandwiches[i]){
-        students.removeAt(j);
+      if(students[j]==sandwiches[i]&&map[j]==null){
+        map[j]=true;
         i++;
         j=0;
       }else j++;
     }
-    return students.length;
+    return students.length-map.keys.length;
   }
 }
