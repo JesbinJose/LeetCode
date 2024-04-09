727f33f52cc94e5679ef7830a496694218fd76f7
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-09 20:14:41 +0530

    Time: 385 ms (8.51%) | Memory: 150.5 MB (42.55%) - LeetSync

--- 39-combination-sum/combination-sum.dart
+++ 39-combination-sum/combination-sum.dart
@@ -2,7 +2,7 @@ class Solution {
   List<List<int>> combinationSum(List<int> c, int target) {
     final List<List<int>> res = [];
     void rec(int target, int startIndex, List<int> current) {
-      if (target == 0) res.add(List.from(current));
+      if (target == 0) res.add(current);
       if (target <= 0 || startIndex >= c.length) return;
       for (int i = startIndex; i < c.length; i++) {
         rec(target - c[i], i, [...current,c[i]]);
