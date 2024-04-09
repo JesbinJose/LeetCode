f04b29549f7bf0d5c03fd3f94796cd137e319fff
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-09 17:25:03 +0530

    Time: 356 ms (25.00%) | Memory: 147.4 MB (75.00%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.dart
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.dart
@@ -2,8 +2,9 @@ class Solution {
   int timeRequiredToBuy(List<int> t, int k) {
     int res = 0;
     for(int index = 0;t[k]!=0;index++){
-      if(t[index%t.length]!=0){
-        t[index%t.length]--;
+      int i = index%t.length;
+      if(t[i]!=0){
+        t[i]--;
         res++;
       }
     }
