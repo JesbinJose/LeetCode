9685e654ff8f8bc580cc36ca95d92f9eabf30a35
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-09 18:03:47 +0530

    Time: 373 ms (50.00%) | Memory: 150.2 MB (50.00%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.dart
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.dart
@@ -1,10 +1,11 @@
 class Solution {
   int kthFactor(int n, int k) {
-    int i = 0;
-    while(i++<=n){
-      if(n%i==0) k--;
+    int i = 0,l = n~/2;
+    while(i<=l){
+      if(n%++i==0) k--;
       if(k==0) return i;
     }
+    if(k==1) return n;
     return -1;
   }
 }
