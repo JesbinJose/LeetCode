eec3552e89cb552aeead8dbf02e2424d10f8cbef
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-12 13:53:35 +0530

    Time: 360 ms (25.00%) | Memory: 149.2 MB (65.00%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.dart
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.dart
@@ -1,12 +1,28 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *   int val;
- *   TreeNode? left;
- *   TreeNode? right;
- *   TreeNode([this.val = 0, this.left, this.right]);
- * }
- */
+import 'dart:collection';
 class Solution {
-  TreeNode? sortedArrayToBST(List<int> nums) => nums.isNotEmpty?TreeNode(nums[nums.length ~/ 2],sortedArrayToBST(nums.sublist(0, nums.length ~/ 2)),sortedArrayToBST(nums.sublist(nums.length ~/ 2 + 1, nums.length),)):null;
+  TreeNode? sortedArrayToBST(List<int> nums) {
+    Queue<List<dynamic>> queue = Queue();
+    TreeNode? root;
+    int left = 0;
+    int right = nums.length - 1;
+    int mid = (left + right) ~/ 2;
+    root = TreeNode(nums[mid]);
+    queue.add([root, left, mid - 1]);
+    queue.add([root, mid + 1, right]);
+    while (queue.isNotEmpty) {
+      var nodeRange = queue.removeFirst();
+      TreeNode? parent = nodeRange[0];
+      int leftIdx = nodeRange[1];
+      int rightIdx = nodeRange[2];
+      if (leftIdx <= rightIdx) {
+        mid = (leftIdx + rightIdx) ~/ 2;
+        TreeNode newNode = TreeNode(nums[mid]);
+        if (nums[mid] < parent!.val) parent.left = newNode;
+        else parent.right = newNode;
+        queue.add([newNode, leftIdx, mid - 1]);
+        queue.add([newNode, mid + 1, rightIdx]);
+      }
+    }
+    return root;
+  }
 }
