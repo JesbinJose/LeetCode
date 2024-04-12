c51160fd7781ea1cebfb8e2456af2c96759054df
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-12 09:40:37 +0530

    Time: 309 ms (80.26%) | Memory: 149.1 MB (51.32%) - LeetSync

--- 75-sort-colors/sort-colors.dart
+++ 75-sort-colors/sort-colors.dart
@@ -2,7 +2,7 @@ class Solution {
   void sortColors(List<int> nums) {
     for(int i=0,r=0,b=nums.length-1;i<=b;i++){
       if(nums[i]==0){
-        nums[i]=nums[r];
+        nums[i] = 1;
         nums[r++]=0;
       }else if(nums[i]==2){
         nums[i--]=nums[b];
