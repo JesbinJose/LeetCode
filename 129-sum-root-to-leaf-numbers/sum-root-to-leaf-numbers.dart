fae45d192f3ef34464ea5ad52e327c87542257fe
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-15 11:27:32 +0530

    Time: 327 ms (71.43%) | Memory: 149.4 MB (42.86%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.dart
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  int sumNumbers(TreeNode? root,[int curSum = 0]) {
+    if(root==null)return 0;
+    int sum = (curSum*10)+ root.val;
+    if(root.left==null&&root.right==null) return sum;
+    return sumNumbers(root?.left,sum)+sumNumbers(root?.right,sum);
+  }
+}
