7c99eed1445836fe3649908b7636f1d0281c2b39
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-15 11:28:15 +0530

    Time: 341 ms (28.57%) | Memory: 152.8 MB (14.29%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.dart
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.dart
@@ -9,7 +9,7 @@
  */
 class Solution {
   int sumNumbers(TreeNode? root,[int curSum = 0]) {
-    if(root==null)return 0;
+    if(root==null) return 0;
     int sum = (curSum*10) + root.val;
     if(root.left==null&&root.right==null) return sum;
     return sumNumbers(root?.left,sum)+sumNumbers(root?.right,sum);
