5f32483b9535ff678af439bcdddbafc28df0dd27
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-16 20:33:07 +0530

    Time: 368 ms (54.55%) | Memory: 152.1 MB (18.18%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.dart
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.dart
@@ -4,10 +4,9 @@ class Solution {
     ListNode dummy = ListNode(0,head);
     ListNode prevGroupEnd = dummy;
     while (true) {
-      ListNode? groupStart = prevGroupEnd.next;
-      ListNode? end = groupStart;
-      ListNode? prev = null;
-      for (int i = 0; i < k; i++,end = end.next) if (end == null) return dummy.next;
+      var [groupStart,prev] = [prevGroupEnd.next, null];
+      for (int i = 0; i < k; i++,groupStart = groupStart.next) if (groupStart == null) return dummy.next;
+      groupStart = prevGroupEnd.next;
       for (int i = 0; i < k; i++) {
         ListNode? nextNode = groupStart!.next;
         groupStart.next = prev;
