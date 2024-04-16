0576096a247694cc1f6043a6c230dda41579d7b4
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-16 17:35:01 +0530

    Time: 320 ms (25.00%) | Memory: 148.7 MB (50.00%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.dart
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.dart
@@ -1,11 +1,11 @@
 class Solution {
   TreeNode? bstToGst(TreeNode? root) {
-    int helper(TreeNode? root,int sum){
-      if(root==null) return sum;  
-      root.val += helper(root.right,sum);
-      return helper(root.left,root.val);
-    }
     helper(root,0);
     return root;
+  }
+  int helper(TreeNode? root,int sum){
+    if(root==null) return sum;  
+    root.val += helper(root.right,sum);
+    return helper(root.left,root.val);
   }
 }
