255f9d94a590f44d5586c38f142f6cbdd8dd0ac6
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-17 12:51:59 +0530

    Time: 473 ms (14.29%) | Memory: 155.9 MB (71.43%) - LeetSync

--- 1708-design-parking-system/design-parking-system.dart
+++ 1708-design-parking-system/design-parking-system.dart
@@ -4,19 +4,13 @@ class ParkingSystem {
   bool addCar(int carType) {
     switch(carType){
       case 1:
-      return big-->0;
+        return big-->0;
       case 2:
-      return medium-->0;
+        return medium-->0;
       case 3:
-      return small-->0;
+        return small-->0;
       default:
-      return false;
+        return false;
     }
   }
 }
-
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem obj = ParkingSystem(big, medium, small);
- * bool param1 = obj.addCar(carType);
- */
