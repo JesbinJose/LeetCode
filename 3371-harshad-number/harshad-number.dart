ae5cdbd9b845ad1d17316ad46e30c1dc5252ac82
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-18 09:49:24 +0530

    Time: 291 ms (100.00%) | Memory: 152.5 MB (5.26%) - LeetSync

--- 3371-harshad-number/harshad-number.dart
+++ 3371-harshad-number/harshad-number.dart
@@ -1,6 +1,6 @@
 class Solution {
   int sumOfTheDigitsOfHarshadNumber(int x) {
-    if(x==100) return 1;
+    if( x==100 ) return 1;
     int sum = x%10 + x~/10;
     return x % sum == 0?sum:-1;
   }
