f124886cc52164c3369370d5d756052398d00a97
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-19 10:14:56 +0530

    Time: 341 ms (31.03%) | Memory: 146.1 MB (96.55%) - LeetSync

--- 50-powx-n/powx-n.dart
+++ 50-powx-n/powx-n.dart
@@ -1,3 +1,17 @@
 class Solution {
-  double myPow(double x, int n) => pow(x,n).toDouble();
+  double myPow(double x, int n) {
+    if (n == 0) return 1;
+    double res = 1;
+    bool isNegative = n < 0;
+    if (isNegative) {
+      x = 1 / x;
+      n = -n;
+    }
+    while (n > 0) {
+      if (n % 2 == 1) res *= x;
+      x *= x;
+      n ~/= 2;
+    }
+    return res;
+  }
 }
