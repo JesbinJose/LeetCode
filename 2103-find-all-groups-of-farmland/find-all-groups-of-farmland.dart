2b75782c790d510474c7b188e75f5b457c5c3bbc
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-20 10:50:43 +0530

    Time: 392 ms (100.00%) | Memory: 192.8 MB (100.00%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.dart
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.dart
@@ -4,8 +4,8 @@ class Solution{
     for (int i = 0; i < land.length; i++) {
       for (int j = 0; j < land[i].length; j++) {
         if (land[i][j] == 1) {
-          int rowEnd = i, colEnd = j;  
-          while (rowEnd < land.length && land[rowEnd ][j] == 1) rowEnd++;
+          int rowEnd = i + 1, colEnd = j + 1;  
+          while (rowEnd < land.length && land[rowEnd][j] == 1) rowEnd++;
           while (colEnd < land[i].length && land[i][colEnd ] == 1) colEnd++;
           farms.add([i, j, --rowEnd, --colEnd]);
           for (int k = i; k <= rowEnd; k++) for (int l = j; l <= colEnd; l++) land[k][l] = -1;
