672e4d7f816d901504830033c4703603f2734d06
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-26 13:24:44 +0530

    Time: 391 ms (100.00%) | Memory: 158.6 MB (100.00%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.dart
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.dart
@@ -1,12 +1,11 @@
 class Solution {
   int minFallingPathSum(List<List<int>> grid) {
     final int n = grid.length;
-    final int dp = 100000000000; 
-    final List<List<int>> dpTable = List.generate(n, (_) => List.filled(n, dp));
+    final List<List<int>> dpTable = List.generate(n, (_) => List.filled(n, 100000000000));
     for (int i = 0; i < n; i++) dpTable[n - 1][i] = grid[n - 1][i];
     for (int row = n - 2; row >= 0; row--) 
       for (int col = 0; col < n; col++) {
-        int minSum = dp;
+        int minSum = 100000000000;
         for (int nextCol = 0; nextCol < n; nextCol++) 
           if (nextCol != col) minSum = min(minSum, dpTable[row + 1][nextCol]);
         dpTable[row][col] = grid[row][col] + minSum;
