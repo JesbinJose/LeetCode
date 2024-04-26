517c0d6c623ebee41050c85b16a615d069fe1314
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-26 20:26:09 +0530

    Time: 325 ms (50.00%) | Memory: 148.9 MB (64.29%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.dart
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.dart
@@ -1,9 +1,9 @@
 class Solution {
   ListNode? swapPairs(ListNode? head) {
-    if (head == null || head.next == null) return head;
-    final ListNode? newHead = head.next;
-    head.next = swapPairs(newHead!.next);
-    newHead.next = head;
+    if (head?.next == null) return head;
+    final ListNode? newHead = head?.next;
+    head?.next = swapPairs(newHead!.next);
+    newHead?.next = head;
     return newHead;
   }
 }
