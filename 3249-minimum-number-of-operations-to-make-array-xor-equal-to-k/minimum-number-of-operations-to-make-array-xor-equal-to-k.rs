7f849a685cc4f98ba1387da87235f480f7da6dd7
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-04-29 19:46:39 +0530

    Time: 7 ms (100.00%) | Memory: 3.5 MB (80.00%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut xor_res: i32 = k;
+        let mut res: i32 = 0;
+        for n in nums {
+            xor_res ^= n;
+        }
+        while xor_res > 0 {
+            if xor_res & 1 == 1 {
+                res += 1;
+            }
+            xor_res >>= 1;
+        }
+        res 
+    }
+}
