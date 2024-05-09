aa4165e4dc47dadb891a5d766dd88cfaa407426e
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-09 07:41:26 +0530

    Time: 570 ms (100.00%) | Memory: 193.7 MB (33.33%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.dart
@@ -0,0 +1,10 @@
+class Solution {
+  int maximumHappinessSum(List<int> happiness, int k) {
+    happiness.sort();
+    int l = happiness.length-k-1, res = 0,m=0;
+    for(int i = happiness.length-1;i>l;i--){
+      res = max(res,res+happiness[i]-m++);
+    }
+    return res;
+  }
+}
