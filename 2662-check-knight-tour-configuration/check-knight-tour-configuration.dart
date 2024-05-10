03d48652afa8efd420e78d08da914c292b90155c
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-10 09:30:07 +0530

    Time: 380 ms (100.00%) | Memory: 146.4 MB (100.00%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.dart
@@ -0,0 +1,29 @@
+class Solution {
+  bool checkValidGrid(List<List<int>> grid) {
+    int n = grid.length;
+    List<List<int>> moves = [
+      [2, 1],
+      [2, -1],
+      [-2, 1],
+      [-2, -1],
+      [1, 2],
+      [1, -2],
+      [-1, 2],
+      [-1, -2]
+    ];
+    bool isValidMove(int x, int y,int count) => x >= 0 && x < n && y >= 0 && y < n && grid[x][y]==count;
+    bool dfs(int x, int y, int count) {
+      if (count == n * n) return true;
+      for (var move in moves) {
+        int newX = x + move[0];
+        int newY = y + move[1];
+        if (isValidMove(newX, newY,count)) {
+          if (dfs(newX, newY, count + 1)) return true;
+        }
+      }
+      return false;
+    }
+    if(grid[0][0]!=0) return false;
+    return dfs(0, 0, 1); 
+  }
+}
