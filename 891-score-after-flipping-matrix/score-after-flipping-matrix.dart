5ac26a0b17eb529a699f8344ed3440480c9f19f2
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-13 11:17:34 +0530

    Time: 343 ms (50.00%) | Memory: 152.1 MB (100.00%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.dart
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.dart
@@ -4,11 +4,10 @@ class Solution {
   int matrixScore(List<List<int>> grid) {
     int m =grid.length,n=grid[0].length;
     for(int i = 0;i<m;i++) if(grid[i][0]==0) changeRow(grid[i]);
-    int res = m * pow(2,n) as int;
-    for(int i=1;i<n;i++){
-      int oneCount = 0;
-      int zeroCount = 0;
-      for(int j =0;j<m;j++) grid[j][i] == 1 ? oneCount++ : zeroCount++;
+    int res = m * pow(2, n) as int;
+    for(int i = 1 ; i < n ; i++){
+      int oneCount = 0,zeroCount = 0;
+      for(int j = 0 ; j < m ; j++) grid[j][i] == 1 ? oneCount++ : zeroCount++;
       res += max(oneCount,zeroCount) * pow(2,n-i) as int;
     }
     return res~/2;
