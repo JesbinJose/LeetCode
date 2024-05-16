eceabc24b07857e80d35411e2c9958518a0cf32b
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-16 15:05:41 +0530

    Time: 440 ms (50.00%) | Memory: 176.7 MB (33.33%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.dart
@@ -0,0 +1,23 @@
+class Solution {
+  List<List<String>> partition(String s) {
+    List<List<String>> res = [];
+    List<String> temp = [];
+    void helper(int index){
+      if(index==s.length) res.add([...temp]);
+      else
+        for(int i = index ; i < s.length ; i++)
+          if(isPlaidrome(s,index,i)){
+            temp.add(s.substring(index,i+1));
+            helper(i+1);
+            temp.removeLast();
+          }
+    }
+    helper(0);
+    return res;
+  }
+  
+  bool isPlaidrome(String s,int l,int r){
+    while(l<r) if(s[r--]!=s[l++]) return false;
+    return true;
+  }
+}
