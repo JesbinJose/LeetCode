f6e42221d021807188d7f904e5b8537c34c57dfe
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-19 12:06:43 +0530

    Time: 370 ms (37.50%) | Memory: 151.6 MB (14.29%) - LeetSync

--- 263-ugly-number/ugly-number.dart
+++ 263-ugly-number/ugly-number.dart
@@ -1,10 +1,13 @@
 class Solution {
   bool isUgly(int n) {
     if(n <= 0) return false;
+    while(true){
+      if(n%2==0) n~/=2;
+      else if(n%3==0) n~/=3;
+      else if(n%5==0) n~/=5;
+      else break;
+    }
     if(n == 1) return true;
-    if(n%2==0) return isUgly(n~/2);
-    if(n%3==0) return isUgly(n~/3);
-    if(n%5==0) return isUgly(n~/5);
     return false;
   }
 }
