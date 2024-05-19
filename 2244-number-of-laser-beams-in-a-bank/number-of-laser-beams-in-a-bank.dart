c1990de03731152c88e119aa3c2c9f66dd521c6a
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-19 13:12:21 +0530

    Time: 411 ms (25.00%) | Memory: 157.7 MB (25.00%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int numberOfBeams(List<String> bank) {
+    int res = 0;
+    int preLazer = 0;
+    for(String row in bank.reversed){
+      int curLazer = 0;
+      for(String i in row.split('')) if(i=='1') curLazer++;
+      res += curLazer * preLazer;
+      if(curLazer!=0) preLazer = curLazer;
+    }
+    return res;
+  }
+}
