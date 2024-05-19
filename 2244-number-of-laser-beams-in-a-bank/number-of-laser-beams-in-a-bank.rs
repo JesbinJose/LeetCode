501bcd13f4e513736eaa9fed9e1df1dda7d94e63
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-19 13:21:22 +0530

    Time: 9 ms (8.33%) | Memory: 2.9 MB (8.33%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.rs
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.rs
@@ -1,7 +1,6 @@
 impl Solution {
     pub fn number_of_beams(mut bank: Vec<String>) -> i32 {
         let (mut res,mut preLazer)= (0,0);
-        bank.reverse();
         for row in bank {
             let mut curLazer = 0;
             for i in row.chars() {
