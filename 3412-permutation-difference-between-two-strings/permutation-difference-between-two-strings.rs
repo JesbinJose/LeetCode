6d6a8bc7132e40caeb44b0b17f4a81a3374f9928
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-19 12:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 2 MB (78.00%) - LeetSync

---
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn find_permutation_difference(s: String, t: String) -> i32 {
+        let mut res: i32 = 0;
+        for (i,v) in s.chars().enumerate() {
+            if let Some(index) = t.find(v) {
+                res += (i as i32 - index as i32).abs();
+            } 
+        }
+        res
+    }
+}
