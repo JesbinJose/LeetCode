130e2cdafd1188b645e1bee81a4a8fe22dae420d
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-05-21 13:17:01 +0530

    Time: 360 ms (6.25%) | Memory: 151.1 MB (12.50%) - LeetSync

--- 78-subsets/subsets.dart
+++ 78-subsets/subsets.dart
@@ -4,9 +4,9 @@ class Solution {
   List<List<int>> subsets(List<int> nums) {
     List<List<int>> result = List.generate(pow(2, nums.length) as int, (_) => []);
     int index = 1;
-    for (int num in nums) {
+    for (int n in nums) {
       int length = index;
-      for (int i = 0; i < length; i++) result[index++] = [...result[i], num];
+      for (int i = 0; i < length; i++) result[index++] = [...result[i], n];
     }
     return result;
   }
