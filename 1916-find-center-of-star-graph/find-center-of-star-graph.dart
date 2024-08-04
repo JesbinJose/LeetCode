14b821f1175d12700abbd435a90351285b709da0
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-08-04 16:43:13 +0530

    Time: 387 ms (70.49%) | Memory: 178.7 MB (81.15%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.dart
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.dart
@@ -1,9 +1,5 @@
 class Solution {
   int findCenter(List<List<int>> edges) {
-    if(edges[1].contains(edges[0][1])){
-      return edges[0][1];
-    }else{
-      return edges[0][0];
-    }
+    return edges[1].contains(edges[0][1]) ? edges[0][1] : edges[0][0];
   }
 }
