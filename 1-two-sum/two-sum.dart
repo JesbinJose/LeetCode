eb48e6826b0408cb021ec91cb8cb26ddefbc4778
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-08-04 12:33:05 +0530

    Time: 338 ms (90.14%) | Memory: 149.9 MB (50.51%) - LeetSync

--- 1-two-sum/two-sum.dart
+++ 1-two-sum/two-sum.dart
@@ -1,13 +1,11 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    int right = 0;
-    int left = nums.length-1;
-    List<int> temp = [...nums];
-    nums.sort();
-    while(right<left){
-      if(nums[right] + nums[left] == target) return [temp.indexOf(nums[right]),temp.lastIndexOf(nums[left])];
-      nums[right] + nums[left] > target ? left-- : right++;
+    final Map<int, int> map = {};
+    for (int x = 0; x <= nums.length - 1; x++) {
+      int remains = target - nums[x];
+      if (map.containsKey(remains)) return [x, map[remains]!];
+      map[nums[x]] = x;
     }
-    return [0,0];
+    return [];
   }
 }
