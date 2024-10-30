58b9a1187ed73b52ef458f7ee45dbb9949908fdf
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2024-10-30 22:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (61.44%) - LeetSync

--- 1-two-sum/two-sum.rs
+++ 1-two-sum/two-sum.rs
@@ -4,14 +4,16 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map: HashMap<i32, i32> = HashMap::new();
         
-        for (index, &value) in nums.iter().enumerate() {
-            let diff: i32 = target - value;
-            if let Some(&prev_index) = map.get(&diff) {
-                return vec![prev_index, index as i32];
+        for (index, value) in nums.iter().enumerate() {
+            let complement = target - value;
+            match map.get(&complement) {
+                Some(&v) => return vec![v, index as i32],
+                None => {
+                    map.insert(*value, index as i32);
+                }
             }
-            map.insert(value, index as i32);
         }
         
-        vec![]
+        vec![] 
     }
 }
