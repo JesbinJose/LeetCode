53a0a42b8905f8c2f0efc5ec77e99760ec0f9ed1
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2025-02-21 00:46:21 +0530

    Time: 3 ms (98.10%) | Memory: 151 MB (42.09%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.dart
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.dart
@@ -2,15 +2,16 @@ import 'dart:math';
 
 class Solution {
   int lengthOfLongestSubstring(String s) {
-    Map<String, int> freq = {};
+    List<int> lastIndex = List.filled(95, -1);
     int maxLen = 0, start = 0;
 
     for (int end = 0; end < s.length; end++) {
-      if (freq.containsKey(s[end]) && freq[s[end]]! >= start) {
-        start = freq[s[end]]! + 1;
+      int index = s.codeUnitAt(end) - 32;
+      if(lastIndex[index]>=start){
+        start = lastIndex[index] + 1;
       }
-      freq[s[end]] = end;
-      maxLen = max(maxLen, end - start + 1);
+      lastIndex[index] = end;
+      maxLen = max(maxLen,end - start + 1);
     }
 
     return maxLen;
