df21f138bcb3790ee06b86f2f76cc385a4885e82
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2025-02-20 23:33:38 +0530

    Time: 6 ms (81.68%) | Memory: 150.9 MB (35.49%) - LeetSync

--- 1-two-sum/two-sum.dart
+++ 1-two-sum/two-sum.dart
@@ -1,10 +1,10 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
     Map<int,int> map = {};
-    for(var (index,value) in nums.indexed){
-      if(map[value] != null) return [map[value]!,index];
-      int left = target - value;
-      map[left] = index;
+    for(int i = 0;i<nums.length;i++){
+        if(map[nums[i]] != null) return [map[nums[i]]!,i];
+        int rem = target - nums[i];
+        map[rem] = i;
     }
     return [];
   }
