b0aec7130b189d29d0f76b55bb2820b6ebd2d3f7
Jesbin <96540097+JesbinJose@users.noreply.github.com>
2025-02-22 00:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 150.5 MB (29.79%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.dart
+++ 3379-score-of-a-string/score-of-a-string.dart
@@ -1,11 +1,9 @@
 class Solution {
   int scoreOfString(String s) {
-    int res = 0 , pre = s.codeUnitAt(0);
-    for(int i =1;i<s.length;i++){
-      int cur = s.codeUnitAt(i);
-      res += (pre-cur).abs();
-      pre = cur;
+    int ans = 0;
+    for(int i = 1;i<s.length;i++){
+        ans += (s.codeUnitAt(i-1) - s.codeUnitAt(i)).abs();
     }
-    return res;
+    return ans;
   }
 }
